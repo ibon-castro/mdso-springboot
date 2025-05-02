8867f99a9199af001cfb04005613b15e9755d079
ibon-castro <69823509+ibon-castro@users.noreply.github.com>
2025-05-02 11:14:25 +0200

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-cd /home/ec2-user/app
-nohup java -jar my-app.jar > app.log 2>&1 &
+
+cd /home/ec2-user/app/target || exit
+
+# Ensure any previous app is stopped (optional safety)
+pkill -f 'java -jar prueba-0.0.1-SNAPSHOT.jar'
+
+# Start the app
+nohup java -jar prueba-0.0.1-SNAPSHOT.jar > app.log 2>&1 &
+
+# Optional: Write the PID to a file for stop.sh
+echo $! > app.pid
